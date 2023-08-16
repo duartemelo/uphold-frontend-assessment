5a391f3656f12ecc5f4ba4079ceba054a7d3dd3b
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-08-16 16:43:11 +0100

    Rates container component

---
+++ src/components/organism/RatesContainer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function RatesContainer() {
+  return (
+    <div>
+      <p className="text-center mt-6 text-gray-400">
+        Enter an amount to check the rates.
+      </p>
+      <div className="h-[500px]" />
+    </div>
+  );
+}
+
+export default RatesContainer;
